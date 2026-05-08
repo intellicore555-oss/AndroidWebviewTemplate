3322a741ad88a2015f5f4b3065c892baaf321014
intellicore555-oss <intellicore555@gmail.com>
2026-05-08 11:14:45 -0300

    Update LocalizacaoWidget.kt

--- 01.WebViewStarterTemplate/app/src/main/java/com/mulheres/LocalizacaoWidget.kt
+++ 01.WebViewStarterTemplate/app/src/main/java/com/mulheres/LocalizacaoWidget.kt
@@ -1,5 +1,53 @@
 package com.mulheres
 
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.widget.RemoteViews
 
-class LocalizacaoWidget : AppWidgetProvider()
+class LocalizacaoWidget : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+
+        for (appWidgetId in appWidgetIds) {
+
+            val views = RemoteViews(
+                context.packageName,
+                R.layout.widget_localizacao
+            )
+
+            // GOOGLE MAPS
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("geo:0,0?q=Minha+Localizacao")
+            )
+
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+
+            val pendingIntent = PendingIntent.getActivity(
+                context,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT or
+                PendingIntent.FLAG_IMMUTABLE
+            )
+
+            views.setOnClickPendingIntent(
+                R.id.localizacao,
+                pendingIntent
+            )
+
+            appWidgetManager.updateAppWidget(
+                appWidgetId,
+                views
+            )
+        }
+    }
+}
