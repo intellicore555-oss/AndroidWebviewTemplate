2d5eaa35e5eddd156ac474e45ff8649b7d129e27
intellicore555-oss <intellicore555@gmail.com>
2026-05-08 11:14:12 -0300

    Update SosWidget.kt

--- 01.WebViewStarterTemplate/app/src/main/java/com/mulheres/SosWidget.kt
+++ 01.WebViewStarterTemplate/app/src/main/java/com/mulheres/SosWidget.kt
@@ -1,5 +1,57 @@
 package com.mulheres
 
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.widget.RemoteViews
 
-class SosWidget : AppWidgetProvider()
+class SosWidget : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+
+        for (appWidgetId in appWidgetIds) {
+
+            val views = RemoteViews(
+                context.packageName,
+                R.layout.widget_sos
+            )
+
+            // NUMERO
+            val numero = "180"
+
+            // LIGAÇÃO
+            val intent = Intent(
+                Intent.ACTION_CALL,
+                Uri.parse("tel:$numero")
+            )
+
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+
+            val pendingIntent = PendingIntent.getActivity(
+                context,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT or
+                PendingIntent.FLAG_IMMUTABLE
+            )
+
+            // CLICK
+            views.setOnClickPendingIntent(
+                R.id.sos,
+                pendingIntent
+            )
+
+            appWidgetManager.updateAppWidget(
+                appWidgetId,
+                views
+            )
+        }
+    }
+}
