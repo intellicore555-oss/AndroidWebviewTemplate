c618531818f92a3e43c9ef1e7344682bfecce53d
Mulher amparada <mulheramparada@gmail.com>
2026-05-18 12:53:24 -0300

    Update MainActivity.kt

--- 02. ArtUi/app/src/main/java/com/mulheres/MainActivity.kt
+++ 02. ArtUi/app/src/main/java/com/mulheres/MainActivity.kt
@@ -131,6 +131,14 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
 
+        window.decorView.systemUiVisibility =
+    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+    View.SYSTEM_UI_FLAG_FULLSCREEN or
+    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        
         setContentView(R.layout.activity_main)
 
         webView = findViewById(R.id.webview)
