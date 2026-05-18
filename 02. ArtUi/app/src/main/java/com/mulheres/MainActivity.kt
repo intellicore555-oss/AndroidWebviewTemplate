395242b3fd40ac711d9ef2c88c0f675ff8cf6383
Mulher amparada <mulheramparada@gmail.com>
2026-05-18 13:03:12 -0300

    Update MainActivity.kt

--- 02. ArtUi/app/src/main/java/com/mulheres/MainActivity.kt
+++ 02. ArtUi/app/src/main/java/com/mulheres/MainActivity.kt
@@ -129,27 +129,46 @@ class MainActivity : AppCompatActivity() {
     // ==========================
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        super.onCreate(savedInstanceState)
+    super.onCreate(savedInstanceState)
 
-        window.decorView.systemUiVisibility =
-    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-    View.SYSTEM_UI_FLAG_FULLSCREEN or
-    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        
-        setContentView(R.layout.activity_main)
+    window.decorView.setBackgroundColor(
+        android.graphics.Color.BLACK
+    )
 
-        webView = findViewById(R.id.webview)
+    window.statusBarColor =
+        android.graphics.Color.BLACK
 
-        configurarWebView()
+    window.navigationBarColor =
+        android.graphics.Color.BLACK
 
-        if (!temPermissoes()) {
-            pedirPermissoes()
-        } else {
-            carregarWebView()
-        }
+    window.decorView.systemUiVisibility =
+        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+        View.SYSTEM_UI_FLAG_FULLSCREEN or
+        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+
+    supportActionBar?.hide()
+
+    setContentView(R.layout.activity_main)
+
+    webView = findViewById(R.id.webview)
+
+    webView.setBackgroundColor(
+        android.graphics.Color.BLACK
+    )
+
+    configurarWebView()
+
+    if (!temPermissoes()) {
+
+        pedirPermissoes()
+
+    } else {
+
+        carregarWebView()
+    }
     }
 
     // ==========================
