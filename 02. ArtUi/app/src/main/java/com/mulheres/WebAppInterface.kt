873d6d07b7dde0d7fa167dad2737e8a27d5cbb51
Mulher amparada <mulheramparada@gmail.com>
2026-05-18 14:05:04 -0300

    Update WebAppInterface.kt

--- 02. ArtUi/app/src/main/java/com/mulheres/WebAppInterface.kt
+++ 02. ArtUi/app/src/main/java/com/mulheres/WebAppInterface.kt
@@ -6,53 +6,59 @@ class WebAppInterface(
     private val activity: MainActivity
 ) {
 
+    private fun ui(block: () -> Unit) {
+        activity.runOnUiThread {
+            block()
+        }
+    }
+
     @JavascriptInterface
     fun carregarWebView() {
-        activity.runOnUiThread { activity.carregarWebView() }
+        ui { activity.carregarWebView() }
     }
 
     @JavascriptInterface
     fun carregarWebView1() {
-        activity.runOnUiThread { activity.carregarWebView1() }
+        ui { activity.carregarWebView1() }
     }
 
     @JavascriptInterface
     fun carregarWebView2() {
-        activity.runOnUiThread { activity.carregarWebView2() }
+        ui { activity.carregarWebView2() }
     }
 
     @JavascriptInterface
     fun carregarWebView3() {
-        activity.runOnUiThread { activity.carregarWebView3() }
+        ui { activity.carregarWebView3() }
     }
 
     @JavascriptInterface
     fun carregarWebView4() {
-        activity.runOnUiThread { activity.carregarWebView4() }
+        ui { activity.carregarWebView4() }
     }
 
     @JavascriptInterface
     fun iniciarBiometria(tipo: Int) {
-        activity.runOnUiThread { activity.iniciarBiometria(tipo) }
+        ui { activity.iniciarBiometria(tipo) }
     }
 
     @JavascriptInterface
     fun iniciarBiometriaAmor() {
-        activity.runOnUiThread { activity.iniciarBiometria(1) }
+        ui { activity.iniciarBiometria(1) }
     }
 
     @JavascriptInterface
     fun iniciarBiometriaMusica() {
-        activity.runOnUiThread { activity.iniciarBiometria(2) }
+        ui { activity.iniciarBiometria(2) }
     }
 
     @JavascriptInterface
     fun iniciarBiometriaPrincesa() {
-        activity.runOnUiThread { activity.iniciarBiometria(3) }
+        ui { activity.iniciarBiometria(3) }
     }
 
     @JavascriptInterface
     fun iniciarBiometriaPrincipe() {
-        activity.runOnUiThread { activity.iniciarBiometria(4) }
+        ui { activity.iniciarBiometria(4) }
     }
 }
